a11a6ed827a38e70a86e7acddd86a9cae54e77c6
Ahmad Zuiod <armousa19@cit.just.edu.jo>
2023-03-25 20:52:57 +0300

    new branch

---
+++ pet-care/src/components/AdoptionPostCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../componentStyle/AdoptionPostCardStyle.css";
+export default function AdoptionPostCard() {
+  let x = 5;
+  const person = {
+    name: "Ahmad",
+    age: "20",
+  };
+
+  return (
+    <div className="adoptioncard">
+      AdoptionPostCard
+      <p>Hello there s</p>
+      <a href="www.gooogle.com">Click me </a>
+    </div>
+  );
+}
--- pet-care/src/pages/AdoptionPost.js
+++ pet-care/src/pages/AdoptionPost.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../PagesStyle/BackGround.css";
-
+import AdoptionPostCard from "../components/AdoptionPostCard";
 export default function AdoptionPost(props) {
-  return <div className="pages-background"></div>;
+  return <div className="pages-background"><AdoptionPostCard/></div>;
 }
