2932f4738cfe5da241f432124ceeae4de09475ff
Ahmad Zuiod <armousa19@cit.just.edu.jo>
2023-03-25 20:20:57 +0300

    post commit

--- pet-care/src/pages/PostPage.js
+++ pet-care/src/pages/PostPage.js
@@ -2,5 +2,5 @@ import React from "react";
 import "../PagesStyle/BackGround.css";
 
 export default function Post(props) {
-  return <div className="pages-background"></div>;
+  return <div className="pages-background">Hello there  </div>;
 }
