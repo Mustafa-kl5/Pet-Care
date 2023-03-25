62d8f0ba1afa6e6d02df046a55aa7ec973df88b5
Mustafa <mmalowisi19@cit.just.edu.jo>
2023-03-25 20:05:28 +0300

    start work on Login page

--- pet-care/src/pages/LoginPage.js
+++ pet-care/src/pages/LoginPage.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "../PagesStyle/BackGround.css";
+import LoginForm from "../components/LoginForm";
 
 export default function Login(props) {
-  return <div className="pages-background"></div>;
+  return (
+    <div className="pages-background">
+      <LoginForm />
+    </div>
+  );
 }
--- pet-care/src/shaerdComponents/InputFiled.js
+++ pet-care/src/shaerdComponents/InputFiled.js
@@ -2,5 +2,7 @@ import React from "react";
 import "../shaerdComponentStyle/inputFIledStyle.css";
 
 export default function InputFiled(props) {
-  return <input className="input-filed-shape" placeholder={props.name} />;
+  return (
+    <input className="input-filed-shape" placeholder={props.placeholder} />
+  );
 }
