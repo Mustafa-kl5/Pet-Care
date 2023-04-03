7d5b5d533203b82e5378691b11fb6e82c1ae8bbe
Mustafa <mmalowisi19@cit.just.edu.jo>
2023-04-03 03:34:49 +0300

    make Menu navigation

--- pet-care/src/pages/HomePage.js
+++ pet-care/src/pages/HomePage.js
@@ -1,6 +1,11 @@
 import React from "react";
+import NavigationMenu from "../shaerdComponents/NavigationMenu";
 import MainBackGround from "../UI/MainBackGround";
 
 export default function Home(props) {
-  return <MainBackGround></MainBackGround>;
+  return (
+    <MainBackGround>
+      <NavigationMenu />
+    </MainBackGround>
+  );
 }
---
+++ pet-care/src/shaerdComponents/NavigationMenu.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../shaerdComponentStyle/NavigationMenu.css";
+import homeIcon from "../Image/HomeIcon.png";
+import postIcon from "../Image/postBox.png";
+import AdoptionIcon from "../Image/adoptationPost.png";
+import storeIcon from "../Image/store.png";
+import infoIcon from "../Image/information.png";
+import settingIcon from "../Image/setting.png";
+import logOutIcon from "../Image/logout.png";
+
+export default function NavigationMenu() {
+  return (
+    <div className="navigation-holder">
+      <div className="link-item">
+        <Link to={"/Home"} className="link-item-path">
+          <div
+            className="item-icon"
+            style={{ backgroundImage: `url("${homeIcon}")` }}
+          ></div>
+          <div>Home</div>
+        </Link>
+      </div>
+      <div className="link-item">
+        <Link to={"/Posts"} className="link-item-path">
+          <div
+            className="item-icon"
+            style={{ backgroundImage: `url("${postIcon}")` }}
+          ></div>
+          <div>Post</div>
+        </Link>
+      </div>
+      <div className="link-item">
+        <Link to={"/Posts"} className="link-item-path">
+          <div
+            className="item-icon"
+            style={{ backgroundImage: `url("${AdoptionIcon}")` }}
+          ></div>
+          <div>Adoption post</div>
+        </Link>
+      </div>
+      <div className="link-item">
+        <Link to={"/Posts"} className="link-item-path">
+          <div
+            className="item-icon"
+            style={{ backgroundImage: `url("${storeIcon}")` }}
+          ></div>
+          <div>Store</div>
+        </Link>
+      </div>
+      <div className="link-item">
+        <Link to={"/Posts"} className="link-item-path">
+          <div
+            className="item-icon"
+            style={{ backgroundImage: `url("${infoIcon}")` }}
+          ></div>
+          <div>information and Treatment</div>
+        </Link>
+      </div>
+      <div className="link-item">
+        <Link to={"/Posts"} className="link-item-path">
+          <div
+            className="item-icon"
+            style={{ backgroundImage: `url("${settingIcon}")` }}
+          ></div>
+          <div>Profile Setting</div>
+        </Link>
+      </div>
+      <div className="link-item">
+        <Link to={"/Posts"} className="link-item-path">
+          <div
+            className="item-icon"
+            style={{ backgroundImage: `url("${logOutIcon}")` }}
+          ></div>
+          <div>Log Out</div>
+        </Link>
+      </div>
+    </div>
+  );
+}
