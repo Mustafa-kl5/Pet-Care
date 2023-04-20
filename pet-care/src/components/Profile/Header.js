e1e8a08231368ce33f01747fbced65c501cc192a
Ahmad Zuiod <armousa19@cit.just.edu.jo>
2023-04-20 08:40:30 +0300

    finished reset password and change image

---
+++ pet-care/src/components/Profile/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ProfileHeader() {
+  return (
+    <div className="main-header-holder">
+      <div className="profile-logo"></div>
+      <div className="Profile-title">Profile managment</div>
+    </div>
+  );
+}
--- pet-care/src/pages/ProfilePage.js
+++ pet-care/src/pages/ProfilePage.js
@@ -1,11 +1,21 @@
 import React from "react";
+import ProfileHeader from "../components/Profile/Header";
 import ContentHolder from "../UI/ContentHolder";
 import MainBackGround from "../UI/MainBackGround";
-
+import "../componentStyle/ProfilePage/ProfileHeader.css";
+import ProfileImagSection from "../components/Profile/ProfileImagSection";
+import ScrollBar from "../shaerdComponents/ScrollBar";
+import ResetPasswordSection from "../components/Profile/ResetPasswordSection";
 export default function Profile(props) {
   return (
     <MainBackGround>
-      <ContentHolder></ContentHolder>
+      <ContentHolder>
+        <ProfileHeader />
+        <ScrollBar>
+          <ProfileImagSection />
+          <ResetPasswordSection />
+        </ScrollBar>
+      </ContentHolder>
     </MainBackGround>
   );
 }
