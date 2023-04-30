dd4af41b84ed09db57244a8f37a32c164a5f2580
Mustafa <mmalowisi19@cit.just.edu.jo>
2023-04-30 21:11:34 +0300

    update

--- pet-care/src/shaerdComponents/MapPicker.js
+++ pet-care/src/shaerdComponents/MapPicker.js
@@ -1,35 +1,13 @@
 import React, { useState } from "react";
-import LocationPicker from "react-location-picker";
+
 const defaultPosition = {
   lat: 27.9878,
   lng: 86.925,
 };
 export default function MapPicker() {
-  const [locationState, setLocationState] = useState({
-    address: "Kala Pattar Ascent Trail, Khumjung 56000, Nepal",
-    position: {
-      lat: 0,
-      lng: 0,
-    },
-  });
-  const handleLocationChange = ({ position1, address1, places }) => {
-    setLocationState({
-      ...locationState,
-      position: position1,
-      address: address1,
-    });
-  };
   return (
     <div>
-      <h1>{locationState.address}</h1>
-      <div>
-        <LocationPicker
-          containerElement={<div style={{ height: "500px" }} />}
-          mapElement={<div style={{ height: "400px" }} />}
-          defaultPosition={defaultPosition}
-          onChange={handleLocationChange}
-        />
-      </div>
+      <h1></h1>
     </div>
   );
 }
